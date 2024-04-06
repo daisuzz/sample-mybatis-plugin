c68a5c6ee0a3718786c942ca5dbf7edea1f4eae4
daisuzz <daisaku.rugby@gmail.com>
2024-04-06 14:02:54 +0900

    first commit

---
+++ src/main/kotlin/dev/daisuzz/samplemybatisplugin/SampleMybatisPluginApplication.kt
@@ -0,0 +1,11 @@
+package dev.daisuzz.samplemybatisplugin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SampleMybatisPluginApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleMybatisPluginApplication>(*args)
+}
